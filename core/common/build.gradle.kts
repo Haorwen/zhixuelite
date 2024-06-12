13ff488c8d0b396c06cc504fe7e6729871a98bbd
luiooiul <mail@luiooiul.com>
2024-06-12 16:54:26 +0800

    优化

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     alias(libs.plugins.zhixuelite.android.library)
     alias(libs.plugins.zhixuelite.android.hilt)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
     namespace = "com.zhixue.lite.core.common"
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
 }
---
+++ core/common/src/main/kotlin/com/zhixue/lite/core/common/json/JsonString.kt
@@ -0,0 +1,25 @@
+package com.zhixue.lite.core.common.json
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
+
+@Serializable(JsonStringSerializer::class)
+data class JsonString<T>(val json: T)
+
+private class JsonStringSerializer<T>(
+    private val serializer: KSerializer<T>
+) : KSerializer<JsonString<T>> {
+
+    override val descriptor: SerialDescriptor = serializer.descriptor
+
+    override fun serialize(encoder: Encoder, value: JsonString<T>) {}
+
+    override fun deserialize(decoder: Decoder): JsonString<T> {
+        check(decoder is JsonDecoder)
+        return JsonString(decoder.json.decodeFromString(serializer, decoder.decodeString()))
+    }
+}
