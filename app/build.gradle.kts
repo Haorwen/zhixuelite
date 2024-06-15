fab235fa743714bc33918dfce3f42919aed4d572
luiooiul <mail@luiooiul.com>
2024-06-15 09:55:30 +0800

    版本号更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.zhixue.lite"
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.0-alpha01"
         resourceConfigurations += "zh-rCN"
     }
 
